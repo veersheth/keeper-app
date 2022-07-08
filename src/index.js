e760c9426819d693582f80b543a61e4b0b1d18e0
Veer <vtube258@gmail.com>
2022-07-08 11:30:32 +0530

    initialise react app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 //1. Create a new React app.
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./components/App"
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
