f039b00cf50d73333bdf84374fa2faefebe22b88
Veer <vtube258@gmail.com>
2022-07-08 12:19:44 +0530

    create footer component

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
 //4. Create a Footer.jsx component that renders a <footer> element
 //to show a copyright message in a <p> with a dynamically updated year.
 //5. Create a Note.jsx component to show a <div> element with a
