95e8647dbddc0b4edd5d5c8ca53fac6b471928eb
Veer <vtube258@gmail.com>
2022-07-08 12:24:33 +0530

    create sample note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Note(){
+    return <div className="note">
+        <h1>
+            A Title
+        </h1>
+
+        <p>
+            the quick brown fox jumps over the lazy dog
+        </p>
+    </div>
+}
+
+export default Note
