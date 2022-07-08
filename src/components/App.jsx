4c84dd9e19a153f66512d5897863e99a0ef76bf7
Veer <vtube258@gmail.com>
2022-07-08 12:09:47 +0530

    added header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import React from "react"
+import Header from "./Header"
 
 function App() {
-    return <div>working</div>
+    return <div>
+        <Header />
+    </div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-//1. Create a new React app.
-
 import React from "react"
 import ReactDOM from "react-dom"
 
